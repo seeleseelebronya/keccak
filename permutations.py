a71105440b0e11cb2ab391e0caf017ac87231daa
seeleseelebronya <odinliu1217@gmail.com>
2026-09-08 11:29:40 +0800

    done with theta permutation

--- permutations.py
+++ permutations.py
@@ -10,7 +10,7 @@ KeccakState = UInt8[np.ndarray, "5 5 64"]
 def chi(state: KeccakState) -> KeccakState:
     """
     The implementation of the _Chi_ permutation. For more details, see
-    <https://keccak.team/files/Keccak-reference-3.0.pdf> page 15.
+    <https://keccak.team/files/Keccak-reference-3.0.pdf> in page 15.
 
     Parameters:
     -----------
@@ -34,8 +34,47 @@ def chi(state: KeccakState) -> KeccakState:
 
     return updated_state
 
-def theta():
-    ...
+def theta(state: KeccakState) -> KeccakState:
+    """
+    The implementation of the _Theta_ permutation. For more details, see
+    <https://keccak.team/files/Keccak-reference-3.0.pdf> in page 17, or see
+    <https://csrc.nist.gov/files/pubs/fips/202/final/docs/fips_202_draft.pdf>
+    in page 11.
+
+    Parameters:
+    -----------
+    state: KeccakState
+        The Keccak state for the _Theta_ permutation.
+
+    Returns:
+    --------
+    updated_state: KeccakState
+        The state after _Theta_ permutation.
+    """
+    updated_state = np.zeros((5, 5, 64), dtype=np.uint8)
+
+    C = np.zeros((5, 64), dtype=np.uint8)
+    for x in range(5):
+        for z in range(64):
+            C[x, z] = (
+                state[x, 0, z]
+                ^ state[x, 1, z]
+                ^ state[x, 2, z]
+                ^ state[x, 3, z]
+                ^ state[x, 4, z]
+            )
+
+    D = np.zeros((5, 64), dtype=np.uint8)
+    for x in range(5):
+        for z in range(64):
+            D[x, z] = C[(x - 1) % 5, z] ^ C[(x + 1) % 5, (z - 1) % 64]
+
+    for x in range(5):
+        for y in range(5):
+            for z in range(64):
+                updated_state[x, y, z] = state[x, y, z] ^ D[x, z]
+
+    return updated_state
 
 
 def pi():
