26aa3562e7a95f47552a2531571882acf99afa3e
seeleseelebronya <odinliu1217@gmail.com>
2026-09-08 12:33:47 +0800

    done with pi permutation and rho permutation

--- permutations.py
+++ permutations.py
@@ -77,9 +77,59 @@ def theta(state: KeccakState) -> KeccakState:
     return updated_state
 
 
-def pi():
-    ...
+def pi(state: KeccakState) -> KeccakState:
+    """
+    The implementation of the _Pi_ permutation. For more details, see
+    <https://keccak.team/files/Keccak-reference-3.0.pdf> in page 19, or see
+    <https://csrc.nist.gov/files/pubs/fips/202/final/docs/fips_202_draft.pdf>
+    in page 13.
+
+    Parameters:
+    -----------
+    state: KeccakState
+        The Keccak state for the _Pi_ permutation.
+
+    Returns:
+    --------
+    updated_state: KeccakState
+        The state after _Pi_ permutation.
+    """
+    updated_state = np.zeros((5, 5, 64), dtype=np.uint8)
+
+    for x in range(5):
+        for y in range(5):
+            for z in range(64):
+                updated_state[x, y, z] = state[(x + 3 * y) % 5, x, z]
+
+    return updated_state
 
 
-def rho():
-    ...
+def rho(state: KeccakState) -> KeccakState:
+    """
+    The implementation of the _Rho_ permutation. For more details, see
+    <https://keccak.team/files/Keccak-reference-3.0.pdf> in page 21, or see
+    <https://csrc.nist.gov/files/pubs/fips/202/final/docs/fips_202_draft.pdf>
+    in page 12.
+
+    Parameters:
+    -----------
+    state: KeccakState
+        The Keccak state for the _Rho_ permutation.
+
+    Returns:
+    --------
+    updated_state: KeccakState
+        The state after _Rho_ permutation.
+    """
+    updated_state = np.zeros((5, 5, 64), dtype=np.uint8)
+
+    for z in range(64):
+        updated_state[0, 0, z] = state[0, 0, z]
+
+    for t in range(24):
+        x, y = 1, 0
+        for z in range(64):
+            updated_state = state[x, y, (z - (t + 1) * (t + 2) // 2) % 64]
+            x, y = y, 2 * x + 3 * y % 5
+
+    return updated_state
